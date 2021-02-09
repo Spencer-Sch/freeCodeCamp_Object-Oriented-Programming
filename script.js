eecc3635e6ce8ade2227dcea81de158d231a136e
Spencer-Sch <spencer.schoeneman@gmail.com>
2021-02-09 14:15:08 -0600

    Update to JS file
    Added answers for lessons 19 through 25.

--- script.js
+++ script.js
@@ -231,4 +231,134 @@ let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
 
 // Lesson 18
-// 
+// Modify the code so that instances of Dog inherit from Animal.
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
+
+// Lesson 19
+// Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
+
+// Lesson 20
+// Add all necessary code so the Dog object inherits from Animal and the Dog's prototype constructor is set to Dog. Then add a bark() method to the Dog object.
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+}
+
+
+// Only change code above this line
+
+let beagle = new Dog();
+
+// Lesson 21
+// Override the fly() method for Penguin so that it returns "Alas, this is a flightless bird."
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+}
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+// Lesson 22
+// Create a mixin named glideMixin that defines a method named glide. Then use the glideMixin to give both bird and boat the ability to glide.
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Only change code below this line
+
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log("Weeeee, glide!");
+  }
+}
+
+glideMixin(bird);
+glideMixin(boat);
+
+// Lesson 23
+// Change how weight is declared in the Bird function so it is a private variable. Then, create a method getWeight that returns the value of weight 15.
+function Bird() {
+  let weight = 15;
+
+  this.getWeight = function() {
+    return weight;
+  };
+}
+
+// Lesson 24
+// Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
+(function () {
+  console.log("A cozy nest is ready");
+})();
+
+// Lesson 25
+// Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+let funModule = (function () {
+  return {
+    isCuteMixin: function(obj) {
+      obj.isCute = function() {
+        return true;
+      };
+    },
+    singMixin: function(obj) {
+      obj.sing = function() {
+        console.log("Singing to an awesome tune");
+      }
+    }
+  }
+})();
